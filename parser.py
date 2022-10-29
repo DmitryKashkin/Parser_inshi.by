9d3d1bf4327ea2326278cbbae6b7afbbc6204be2
DmitryKashkin <7842066@gmail.com>
2022-10-29 20:31:12 +0300

    v2

--- parser.py
+++ parser.py
@@ -54,16 +54,40 @@ def clear_products_list(products_list: list):
     return cleared_list
 
 
+def get_product(products_list, url_prefix=''):
+    product = {}
+    for item in products_list:
+        item = url_prefix + item.find('a').get('href')
+        response = requests.get(item)
+        soup = BeautifulSoup(response.text, 'lxml')
+        product['name'] = soup.find('h1').text
+        product['normal-price'] = soup.find('p', class_='normal-price').text
+        product['ppack'] = soup.find('p', class_="ppack").text
+        product['nalichie'] = soup.find('div', class_="prod-desc js-product").find_all('p')[2]
+        product['naznachenie'] = soup.find('div', class_="prod-desc js-product").find_all('p')[4]
+        product['naznachenie'] = soup.find('div', class_="prod-desc js-product").find_all('p')[4]
+
+
+        print(product)
+        break
+        # products_list += soup.find_all('span', class_='prod-title')
+
+        # print(item.find('a').get('href'))
+    # print(item.text)
+    # print(item)
+
+
 def main():
     products_list = get_products_list(urls_list)
     products_list = clear_products_list(products_list)
+    get_product(products_list, url_prefix=prefix)
 
     # response = requests.get('https://inshi.by/katalog/remmers')
     # soup = BeautifulSoup(response.text, 'lxml')
     # products_list = soup.find_all('span', class_='prod-title')
 
-    for item in products_list:
-        print(item.find('a').get('href'))
+    # for item in products_list:
+    #     print(item.find('a').get('href'))
     # print(item.text)
     # print(item)
 
