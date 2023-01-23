e661bc70e684bd26827cbc9265094af5efce978f
admin <7842066@gmail.com>
2023-01-23 08:19:25 +0300

    next step

--- yandex_nedvizhimost.py
+++ yandex_nedvizhimost.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from time import sleep
 from selenium import webdriver
@@ -6,12 +7,13 @@ import requests
 from bs4 import BeautifulSoup
 import openpyxl
 import json
-from slugify import slugify
+# from slugify import slugify
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pyppeteer import launch
 
 URL = 'https://realty.ya.ru/moskva_i_moskovskaya_oblast/kupit/novostrojka/?showOutdated=NO'
 CSS_CAPTCHA = '#root > div > div > form > div.Spacer.Spacer_auto-gap_bottom > div > div > div.CheckboxCaptcha-Anchor > input'
@@ -23,7 +25,7 @@ def main_captcha(driver):
     except:
         return
     driver.execute_script("arguments[0].click();", element)
-    sleep(1)
+    sleep(3)
 
 
 # def popup_close(driver):
@@ -37,10 +39,31 @@ def main():
     driver = webdriver.Chrome()
     driver.get(URL)
     main_captcha(driver)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    # print(soup)
+    # sleep(30)
+    while True:
+        # print('while')
+        for item in soup.find_all('div', class_='SiteSnippetSearch SitesSerp__snippet'):
+            # print(item)
+            print(item.a.get('href'))
+            # for item2 in item.find_all('div', class_='Link Link_js_inited Link_size_m Link_theme_islands'):
+            #     print(item2.get('href'))
+        break
+
+    #     'https://realty.ya.ru/moskva_i_moskovskaya_oblast/kupit/novostrojka/?showOutdated=NO&page=48'
+    # print(driver.page_source)
 
 
-    print(driver.page_source)
+# async def main2():
+#     browser = await launch()
+#     # page = await browser.newPage()
+#     # await page.goto('https://example.com')
+#     # await page.screenshot({'path': 'example.png'})
+#     sleep(30)
+#     await browser.close()
 
 
 if __name__ == '__main__':
     main()
+    # asyncio.get_event_loop().run_until_complete(main2())
