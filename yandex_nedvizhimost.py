1bceb0e21021f63f31321a0f0732e66e9aacd7b6
admin <7842066@gmail.com>
2023-01-26 07:32:18 +0300

    next step

--- yandex_nedvizhimost.py
+++ yandex_nedvizhimost.py
@@ -18,18 +18,8 @@ import pickle
 
 URL = 'https://realty.ya.ru/moskva_i_moskovskaya_oblast/kupit/novostrojka/?showOutdated=NO'
 PAGE = 'https://realty.ya.ru/moskva_i_moskovskaya_oblast/kupit/novostrojka/?showOutdated=NO&page='
-URL2 = 'https://dzen.ru/?yredirect=true'
 CSS_CAPTCHA = '#root > div > div > form > div.Spacer.Spacer_auto-gap_bottom > div > div > div.CheckboxCaptcha-Anchor > input'
 PREFIX = 'https://realty.ya.ru'
-
-
-# def load_cookie(driver,url):
-#     try:
-#         for cookie in pickle.load(open('ya_cookies', 'rb')):
-#             driver.add_cookie(cookie)
-#     except FileNotFoundError:
-#         main_captcha(driver,url)
-#         pickle.dump(driver.get_cookies(), open('ya_cookies', 'wb'))
 
 
 def main_captcha(driver, url, s):
@@ -45,56 +35,47 @@ def main_captcha(driver, url, s):
         s.cookies.clear()
         [s.cookies.set(c['name'], c['value']) for c in driver.get_cookies()]
         response = s.get(url)
-        # driver.refresh()
         if 'captcha' in response.text:
             continue
         break
     return response
 
 
-
 def get_spec(url, s, driver):
     spec = {}
-    response = s.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    try:
-        spec['deadline'] = soup.find('span', string='Срок сдачи').next_sibling.text.replace('\xa0', ' ')
-    except:
-        print('!!!!!!!!!!!!!!!!!!!captcha!!!!!!!!!!!!!!!!!!!')
+    while True:
+        response = s.get(url)
+        if response:
+            break
+        continue
+    if 'captcha' in response.text:
         response = main_captcha(driver, url, s)
-        try:
-            soup = BeautifulSoup(response.text, 'lxml')
-        except:
-            print(response)
-            sleep(300)
-        spec['deadline'] = soup.find('span', string='Срок сдачи').next_sibling.text.replace('\xa0', ' ')
-    spec['class'] = soup.find('span', string='Класс жилья').next_sibling.a.text.replace('\xa0', ' ')
-    spec['home_type'] = soup.find('div', string='Тип дома').next_sibling.text.replace('\xa0', ' ')
-    spec['buildings'] = soup.find('div', string='Число корпусов').next_sibling.text.replace('\xa0', ' ')
-    spec['finish'] = soup.find('h2', string='Ещё параметры').next_sibling.contents[1].contents[1].text.replace('\xa0',
-                                                                                                               ' ')
-    spec['queues'] = soup.find('div', string='Очереди').next_sibling.text.replace('\xa0', ' ')
+    soup = BeautifulSoup(response.text, 'lxml')
+    spec['realty_object'] = soup.find('h1', class_='SiteCardHeader__title')
+    spec['deadline'] = soup.find('h2', class_='SiteCardDescription__title--35WGe').next_sibling.find('div', string='Срок сдачи').next_sibling.text.replace('\xa0', ' ')
+    spec['class'] = soup.find('h2', class_='SiteCardDescription__title--35WGe').next_sibling.find('div', string='Класс жилья').next_sibling.text.replace('\xa0', ' ')
+    spec['home_type'] = soup.find('h2', class_='SiteCardDescription__title--35WGe').next_sibling.contents[0].contents[3].contents[1].contents[1].text.replace('\xa0', ' ')
+    spec['buildings'] = soup.find('h2', class_='SiteCardDescription__title--35WGe').next_sibling.contents[0].contents[3].contents[1].contents[1].text.replace('\xa0', ' ')
+    finish = soup.find('h2', string='Ещё параметры').next_sibling.find('div', string='Отделка')
+    if finish:
+        spec['finish'] = finish.next_sibling.text.replace('\xa0', ' ')
+    else:
+        spec['finish'] = 'Нет данных'
+    spec['queues'] = soup.find('h2', string='Ещё параметры').next_sibling.find('div', string='Очереди').next_sibling.text.replace('\xa0', ' ')
     try:
-        spec['num_of_apart'] = soup.find('div', string='Число квартир').next_sibling.text.replace('\xa0', ' ')
+        spec['num_of_apart'] = soup.find('h2', string='Ещё параметры').next_sibling.find('div', string='Число квартир').next_sibling.text.replace('\xa0', ' ')
     except:
-        spec['num_of_apart'] = soup.find('div', string='Число квартир и апартаментов').next_sibling.text.replace('\xa0', ' ')
-
-
-        # print(soup)
-        # print('')
-        # print(url)
-        # with open('products.json', 'w') as fp:
-        #     json.dump(str(soup), fp)
-        # sleep(100)
-
-
+        try:
+            spec['num_of_apart'] = soup.find('h2', string='Ещё параметры').next_sibling.find('div', string='Число квартир и апартаментов').next_sibling.text.replace('\xa0', ' ')
+        except:
+            spec['num_of_apart'] = soup.find('h2', string='Ещё параметры').next_sibling.find('div', string='Число апартаментов').next_sibling.text.replace('\xa0', ' ')
+    print(spec)
 
 
 def main():
     s = requests.Session()
     driver = webdriver.Chrome()
     response = main_captcha(driver, URL, s)
-    # [s.cookies.set(c['name'], c['value']) for c in driver.get_cookies()]
     soup = BeautifulSoup(response.text, 'lxml')
     page_number = 1
     while True:
@@ -104,9 +85,18 @@ def main():
             print(url)
             get_spec(url, s, driver)
         page_number += 1
-        response = s.get(PAGE + str(page_number))
+        url = PAGE + str(page_number)
+        print(url)
+        while True:
+            response = s.get(url)
+            if response:
+                break
+            continue
+        if 'captcha' in response.text:
+            response = main_captcha(driver, url, s)
         if '404' in response:
             break
+        soup = BeautifulSoup(response.text, 'lxml')
 
 
 if __name__ == '__main__':
